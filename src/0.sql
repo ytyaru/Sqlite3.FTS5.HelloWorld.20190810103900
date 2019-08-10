68f48d0d27c552b97b438923db4b0b225f7e15fe
ytyaru <ytyaru@outlook.jp>
2019-08-10 10:55:29 +0900

    初回コミット

---
+++ src/0.sql
@@ -0,0 +1,4 @@
+create virtual table posts using fts5(title, content);
+.tables
+select * from sqlite_master;
+
---
+++ src/2.sql
@@ -0,0 +1,11 @@
+create virtual table posts using fts5(title, content);
+insert into posts values('タイトル1', '本文1');
+insert into posts values('タイトル2', '本文2');
+insert into posts values('たいとる3', 'ほんぶん3');
+insert into posts values('English title 1', 'English content 1.');
+insert into posts values('English title 2', 'English content 2.');
+insert into posts values('english title 3', 'english content 3.');
+
+select * from posts where posts = 'ほんぶん';
+select * from posts where posts = 'ほんぶん3';
+
